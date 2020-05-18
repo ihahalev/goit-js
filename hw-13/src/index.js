268f6c5859e09ac74d79aa44f39903a09c959cc2
ihahalev <i_hahalev@ukr.net>
2020-05-18 15:27:30 +0300

    hw-13 masonry appended load

--- hw-13/src/index.js
+++ hw-13/src/index.js
@@ -1,11 +1,10 @@
-// import './layout';
 import './styles.css';
 import * as PNotify from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 
 import apiService from './apiService';
-import photoCard from './templates/photo-card.hbs';
+import domMarkup from './layout';
 
 PNotify.defaults.width = '400px';
 
@@ -21,15 +20,15 @@ refs.loadMoreBnt.addEventListener('click', loadMoreBntHandler);
 import Masonry from 'masonry-layout';
 import imgLoaded from 'imagesloaded';
 
-// const masonryInstance = new Masonry('.grid', {
-//   columnWidth: '.grid-sizer',
-//   itemSelector: '.grid-item',
-//   percentPosition: true,
-//   transitionDuration: '0.2s',
-//   stagger: 50,
-//   initLayout: true,
-// });
-// const imgLoadedInstance = imgLoaded(document.querySelector('.grid'));
+const masonryInstance = new Masonry('.grid', {
+  columnWidth: '.grid-sizer',
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  transitionDuration: '0.2s',
+  stagger: 50,
+  initLayout: true,
+});
+let imgLoadedInstance = imgLoaded(document.querySelector('.grid'));
 
 function searchFormHandler(e) {
   e.preventDefault();
@@ -46,33 +45,17 @@ async function loadMoreBntHandler(e) {
 
   apiService.incrementPage();
   try {
-    const inserted = await searchQueryHandler(false);
-    if (inserted) {
-      // await setTimeout(
-      //   () =>
-      // const imgLoadedInstance = imgLoaded(document.querySelector('.grid'));
-      // imgLoadedInstance.on('done', () => {
-      //   masonryInstance.layout();
-      //   window.scroll({
-      //     top: window.visualViewport.pageTop + window.innerHeight,
-      //     behavior: 'smooth',
-      //   });
-      // });
-      //   1500,
-      // );
-      // console.log(window);
-      // console.log(window.visualViewport.pageTop + window.innerHeight);
-    }
+    await searchQueryHandler(false);
   } catch (err) {
     console.log(err);
     throw err;
   }
 }
 
-async function searchQueryHandler(info = false) {
+async function searchQueryHandler(firstLoad = false) {
   const collection = await apiService.fetchQuery();
   try {
-    if (info) {
+    if (firstLoad) {
       PNotify.info({
         text: `There are ${collection.total} results`,
       });
@@ -80,60 +63,40 @@ async function searchQueryHandler(info = false) {
     if (collection.total > 12) {
       refs.loadMoreBnt.style.visibility = 'visible';
     }
-    const insertOk = insertListItems(collection.hits);
-    const masonryInstance = new Masonry('.grid', {
-      columnWidth: '.grid-sizer',
-      itemSelector: '.grid-item',
-      percentPosition: true,
-      transitionDuration: '0.2s',
-      stagger: 50,
-      initLayout: true,
-    });
-    const imgLoadedInstance = imgLoaded(document.querySelector('.grid'));
-    imgLoadedInstance.on('progress', () => {
-      masonryInstance.layout();
-      // if (!info) {
-      //   window.scroll({
-      //     top: window.visualViewport.pageTop + window.innerHeight,
-      //     behavior: 'smooth',
-      //   });
-      // }
-    });
+    insertListItems(collection.hits);
+    // const masonryInstance = new Masonry('.grid', {
+    //   columnWidth: '.grid-sizer',
+    //   itemSelector: '.grid-item',
+    //   percentPosition: true,
+    //   transitionDuration: '0.2s',
+    //   stagger: 50,
+    //   initLayout: true,
+    // });
+    // const imgLoadedInstance = imgLoaded(document.querySelector('.grid'));
+    imgLoadedInstance = imgLoaded(document.querySelector('.grid'));
     imgLoadedInstance.on('done', () => {
       masonryInstance.layout();
-      if (!info) {
-        window.scroll({
+      if (!firstLoad) {
+        window.scrollTo({
           top: window.visualViewport.pageTop + window.innerHeight,
           behavior: 'smooth',
         });
       }
     });
-    return insertOk;
   } catch (err) {
     console.log(err);
     throw err;
   }
 }
-//   const collection = apiService.fetchQuery();
-//   collection
-//     .then(data => {
-//       if (info) {
-//         PNotify.info({
-//           text: `There are ${data.total} results`,
-//         });
-//       }
-//       const insertOk = insertListItems(data.hits);
-//       return insertOk;
-//     })
-//     .catch(err => console.log(err));
-// }
 
 function insertListItems(items) {
-  const markup = photoCard(items);
-  refs.gallery.insertAdjacentHTML('beforeend', markup);
-  if (markup) {
-    return true;
-  }
+  const markups = domMarkup(items);
+  refs.gallery.append(...markups);
+  masonryInstance.appended(markups);
+  imgLoadedInstance = imgLoaded(document.querySelector('.grid'));
+  imgLoadedInstance.on('progress', () => {
+    masonryInstance.layout();
+  });
 }
 
 function clearListItems() {
--- hw-13/src/layout.js
+++ hw-13/src/layout.js
@@ -1,7 +1,14 @@
-import Masonry from 'masonry-layout';
+import photoCard from './templates/photo-card.hbs';
 
-const masonryInstance = new Masonry('.grid', {
-  // options
-});
-
-console.log(masonryInstance);
+export default function domMarkup(items) {
+  const markups = items.reduce((acc, item) => {
+    const markup = photoCard(item);
+    const li = document.createElement('li');
+    li.classList.add('grid-item');
+    li.classList.add('gallery__item');
+    li.insertAdjacentHTML('beforeend', markup);
+    acc.push(li);
+    return acc;
+  }, []);
+  return markups;
+}
